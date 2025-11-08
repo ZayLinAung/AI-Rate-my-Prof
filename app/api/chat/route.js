1d393b10d2c0c3febd14144524b2404229c0bfe7
Zacky <89254577+ZayLinAung@users.noreply.github.com>
2025-11-07 22:06:26 -0500

    update gemini model

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -6,7 +6,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GEMINIAI_API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 const embedModel = genAI.getGenerativeModel({ model: "text-embedding-004" });
 
 const systemPrompt = `
